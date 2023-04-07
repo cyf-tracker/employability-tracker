6f456a9a154d2114f4bda8fb250ec09cf020420a
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-04-07 20:09:24 +0100

    connected to db

--- server/app.js
+++ server/app.js
@@ -1,30 +1,46 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
 
-// mongoose
 // helmet
-// dotenv
+
 // cors
 // connect to database
+const connectToDB = async () => {
+	mongoose.set("strictQuery", false);
+	await mongoose
+		.connect("mongodb+srv://" + process.env.MONGO_USERNAME + ":" + process.env.MONGO_PASSWORD + "@" + process.env.MONGO_HOST + "/" + process.env.MONGO_DATABASE_NAME)
+		.then(() => {
+			console.log("Successfully connected to MongoDB Atlas!");
+		})
+		.catch((error) => {
+			console.log("Unable to connect to MongoDB Atlas!");
+			console.error(error);
+		});
+};
+
+connectToDB();
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Request received!');
-  next();
+	console.log("Request received!");
+	next();
 });
 
 app.use((req, res, next) => {
-  res.status(201);
-  next();
+	res.status(201);
+	next();
 });
 
 app.use((req, res, next) => {
-  res.json({ message: 'Your request was successful!' });
-  next();
+	res.json({message: "Your request was successful!"});
+	next();
 });
 
 app.use((req, res, next) => {
-  console.log('Response sent successfully!');
+	console.log("Response sent successfully!");
 });
 
 module.exports = app;
