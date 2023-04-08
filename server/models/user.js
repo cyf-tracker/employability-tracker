f1d128db03319687c1bfe2843673712066aa72ae
Jade-Jones-dev <100792140+Jade-Jones-dev@users.noreply.github.com>
2023-04-08 21:34:35 +0100

    user schema

--- server/models/user.js
+++ server/models/user.js
@@ -1,2 +1,15 @@
-// mongoose model of user
-// mongoose unique validator for email
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const userSchema = mongoose.Schema({
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true},
+	username: {type: String, required: true,unique: true},
+    firstname: {type: String, required: true},
+    surname: {type: String, required: true},
+	isVolunteer: {type: Boolean,required:true, default:false}
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", userSchema);
